004b746becd8a3ab119bbc13acb8c8bc7c0c5b47
Diego Giacomelli <giacomelli@gmail.com>
2016-01-15 20:47:07 -0200

    RandomizationExtensions and BasicRandomization tests improvements.

---
+++ src/GeneticSharp.Domain.UnitTests/Randomizations/RandomizationExtensionsTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeneticSharp.Domain.Randomizations;
+using NUnit.Framework;
+
+namespace GeneticSharp.Domain.UnitTests.Randomizations
+{
+    [TestFixture]
+    public class RandomizationExtensionsTest
+    {
+        [Test]
+        [Category("Randomizations")]
+        public void GetEvenInt_MinMax_OnlyEven()
+        {
+            var rnd = new BasicRandomization();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var actual = rnd.GetEvenInt(0, 10);
+                Assert.IsTrue(actual % 2 == 0);
+            }
+        }
+
+        [Test]
+        [Category("Randomizations")]
+        public void GetOddInt_MinMax_OnlyOdd()
+        {
+            var rnd = new BasicRandomization();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var actual = rnd.GetOddInt(0, 10);
+                Assert.IsTrue(actual % 2 != 0);
+            }
+        }
+    }
+}
