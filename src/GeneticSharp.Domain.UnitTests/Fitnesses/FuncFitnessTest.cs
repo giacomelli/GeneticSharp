c1bde5d7afa7d409f4b21952b5d1b6688e64b129
Diego Giacomelli <giacomelli@gmail.com>
2016-11-27 19:16:20 -0200

    Created the FuncFitness that allow defer the chromosome evaluation to a Func.

---
+++ src/GeneticSharp.Domain.UnitTests/Fitnesses/FuncFitnessTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using GeneticSharp.Domain.Fitnesses;
+
+namespace GeneticSharp.Domain.UnitTests.Fitnesses
+{
+	[TestFixture]
+	public class FuncFitnessTest
+	{
+		[Test]
+		public void Evaluate_Func_CallFunc()
+		{
+			var target = new FuncFitness((c) =>
+			{
+				return c.Fitness.Value + 1;
+			});
+
+			Assert.AreEqual(3, target.Evaluate(new ChromosomeStub(2)));
+		}
+	}
+}
+
