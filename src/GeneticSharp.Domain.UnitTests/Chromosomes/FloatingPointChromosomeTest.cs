c0cdf3af54cfd609e514f261ff6eb6cfc30cee10
Diego Giacomelli <giacomelli@gmail.com>
2016-11-27 19:10:55 -0200

    Created the FloatingPointChromosome.

---
+++ src/GeneticSharp.Domain.UnitTests/Chromosomes/FloatingPointChromosomeTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Randomizations;
+using Rhino.Mocks;
+
+namespace GeneticSharp.Domain.UnitTests
+{
+	[TestFixture]
+	public class FloatingPointChromosomeTest
+	{
+		[Test]
+		public void ToFloatingPoint_NoArgs_Double()
+		{
+			RandomizationProvider.Current = MockRepository.GenerateMock<IRandomization> ();
+			RandomizationProvider.Current.Expect (r => r.GetDouble (0.5, 2.5)).Return (1.1);
+			var target = new FloatingPointChromosome (0.5, 2.5, 2);
+			var actual = target.ToFloatingPoint ();
+
+			Assert.AreEqual (1.1, actual);
+		}
+
+		[Test]
+		public void Constructor_FromZeroToZero_Double()
+		{
+			RandomizationProvider.Current = MockRepository.GenerateMock<IRandomization>();
+			RandomizationProvider.Current.Expect(r => r.GetDouble(0, 0)).Return(0);
+			var target = new FloatingPointChromosome(0, 0, 2);
+			var actual = target.ToFloatingPoint();
+
+			Assert.AreEqual(0, actual);
+		}
+	}
+}
+
---
+++ src/GeneticSharp.Domain/Chromosomes/FloatingPointChromosome.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using GeneticSharp.Domain.Randomizations;
+using System.Text;
+using System.Linq;
+using System.Collections;
+using GeneticSharp.Infrastructure.Framework.Commons;
+
+namespace GeneticSharp.Domain.Chromosomes
+{
+	/// <summary>
+	/// Floating point chromosome with binary values (0 and 1).
+	/// </summary>
+	public class FloatingPointChromosome : BinaryChromosomeBase
+	{
+		private double m_minValue;
+		private double m_maxValue;
+		private int m_decimals;
+		private string m_originalValueStringRepresentation;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:GeneticSharp.Domain.Chromosomes.FloatingPointChromosome"/> class.
+		/// </summary>
+		/// <param name="minValue">Minimum value.</param>
+		/// <param name="maxValue">Max value.</param>
+		/// <param name="decimals">Decimals.</param>
+		public FloatingPointChromosome (double minValue, double maxValue, int decimals) 
+			: base(32)
+		{
+			m_minValue = minValue;
+			m_maxValue = maxValue;
+			m_decimals = decimals;
+			var originalValue = RandomizationProvider.Current.GetDouble (minValue, maxValue);
+			m_originalValueStringRepresentation = BinaryStringRepresentation.ToRepresentation (originalValue, Length, decimals);
+
+			CreateGenes ();
+		}
+
+		/// <summary>
+		/// Creates the new.
+		/// </summary>
+		/// <returns>The new.</returns>
+		public override IChromosome CreateNew ()
+		{
+			return new FloatingPointChromosome (m_minValue, m_maxValue, m_decimals);
+		}
+
+		/// <summary>
+		/// Generates the gene.
+		/// </summary>
+		/// <returns>The gene.</returns>
+		/// <param name="geneIndex">Gene index.</param>
+		public override Gene GenerateGene (int geneIndex)
+		{
+			return new Gene (Convert.ToInt32(m_originalValueStringRepresentation [geneIndex].ToString()));
+		}
+
+		/// <summary>
+		/// Converts the chromosome to the floating point representation.
+		/// </summary>
+		/// <returns>The floating point.</returns>
+		public double ToFloatingPoint()
+		{
+			var stringRepresentation = String.Join ("", GetGenes ().Select (g => g.Value.ToString ()).ToArray());
+			return BinaryStringRepresentation.ToDouble (stringRepresentation);
+		}
+	}
+}
+
