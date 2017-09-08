c397f4c1ebe1ac3d8899d393d72a3e9ecf68ca45
Saner, Can <can.saner@accenture.com>
2017-09-08 11:26:21 +0300

    - Partially Mapped Crossover is fixed. PMX was not working properly because cutPoint may come unsorted.

--- src/GeneticSharp.Domain/Crossovers/PartiallyMappedCrossover.cs
+++ src/GeneticSharp.Domain/Crossovers/PartiallyMappedCrossover.cs
@@ -47,6 +47,7 @@ namespace GeneticSharp.Domain.Crossovers
             var parent2 = parents[1];
 
             var cutPointsIndexes = RandomizationProvider.Current.GetUniqueInts(2, 0, parent1.Length);
+            Array.Sort(cutPointsIndexes);
             var firstCutPointIndex = cutPointsIndexes[0];
             var secondCutPointIdnex = cutPointsIndexes[1];
 
