b62f2db0101b7f3bcd9b557d8c395e8e8f8bcdd7
giacomelli <giacomelli@gmail.comi>
2018-04-28 13:55:10 -0300

    Using IsSleeping

--- src/GeneticSharp.Runner.UnityApp/Assets/_runner/Car/CarController.cs
+++ src/GeneticSharp.Runner.UnityApp/Assets/_runner/Car/CarController.cs
@@ -50,7 +50,7 @@ namespace GeneticSharp.Runner.UnityApp.Car
             {
                 // Car should run at least MinMaxDistanceDiff in the the TimeoutNoBetterMaxDistance seconds,
                 // otherwise its simulation will end
-                if (Chromosome.MaxDistance - lastMaxDistance < m_config.MinMaxDistanceDiff)
+                if (m_rb.IsSleeping() || Chromosome.MaxDistance - lastMaxDistance < m_config.MinMaxDistanceDiff)
                 {
                     StopEvaluation();
                     break;
