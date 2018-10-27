13caf8d085ab47570eeaf37a7ed146441019983b
Jean-Sylvain Boige <jsboige@gmail.com>
2018-10-27 12:24:28 +0200

    Cleaned sudoku unit tests comments

--- src/GeneticSharp.Extensions.UnitTests/Sudoku/SudokuTest.cs
+++ src/GeneticSharp.Extensions.UnitTests/Sudoku/SudokuTest.cs
@@ -15,6 +15,7 @@ using NUnit.Framework;
 namespace GeneticSharp.Extensions.UnitTests.Sudoku
 {
 
+
   [TestFixture()]
   [Category("Extensions")]
   public class SudokuTest
@@ -22,10 +23,13 @@ namespace GeneticSharp.Extensions.UnitTests.Sudoku
 
     private string _easySudokuString = "9.2..54.31...63.255.84.7.6..263.9..1.57.1.29..9.67.53.24.53.6..7.52..3.4.8..4195.";
 
-
+    /// <summary>
+    /// The sample sudoku string should parse properly into corresponding cells
+    /// </summary>
     [Test()]
     public void ParseSudoku()
     {
+
       var sudoku = Extensions.Sudoku.Sudoku.Parse(_easySudokuString);
 
       Assert.AreEqual(sudoku.CellsList[0], 9);
@@ -37,27 +41,27 @@ namespace GeneticSharp.Extensions.UnitTests.Sudoku
     }
 
 
-
+    /// <summary>
+    /// The permutation chromosome should always solve the sudoku in a reasonable time with 1000 chromosomes
+    /// </summary>
     [Test()]
     public void Solve_sudoku_with_permutations()
     {
      var sudoku = Extensions.Sudoku.Sudoku.Parse(_easySudokuString);
 
-      //the permutation chromosome should always solve the sudoku in a reasonable time with 700 chromosomes 
       IChromosome chromosome = new SudokuPermutationsChromosome(sudoku);
       var fitness = EvaluatesSudokuChromosome(chromosome, sudoku, 1000, -1);
       Assert.AreEqual(fitness, 0);
 
     }
 
-
+    /// <summary>
+    /// The cells chromosome might need more individuals, so in order to keep execution time low, we only expect near completion
+    /// </summary>
     [Test()]
     public void Nearly_solve_sudoku_with_Cells()
     {
-      // checking that a simple Sudoku can be tackled using various strategies
       var sudoku = Extensions.Sudoku.Sudoku.Parse(_easySudokuString);
-
-      // Other chromosomes would require more individuals thus more time, so we simply test for significant progresses
 
       //the cells chromosome should solve the sudoku or nearly in less than 50 generations with 500 chromosomes
       var chromosome = new SudokuCellsChromosome(sudoku);
@@ -66,20 +70,21 @@ namespace GeneticSharp.Extensions.UnitTests.Sudoku
 
     }
 
-
+    /// <summary>
+    /// The random permutations chromosome require more individuals and generations, so we only test for significant progresses
+    /// </summary>
     [Test()]
     public void Make_Progresses_with_random_permutations()
     {
-      // checking that a simple Sudoku can be tackled using various strategies
       var sudoku = Extensions.Sudoku.Sudoku.Parse(_easySudokuString);
 
 
-      //the Random permutations chromosome should make significant progresses over 20 generations with 20 individuals
+      //the Random permutations chromosome should make significant progresses over 3 generations with 5 individuals
 
       var chromosome = new SudokuRandomPermutationsChromosome(sudoku, 2, 3);
       var fitness1 = new SudokuFitness(sudoku).Evaluate((ISudokuChromosome) chromosome);
       var fitness2 = EvaluatesSudokuChromosome(chromosome, sudoku, 5, 3);
-      Assert.Greater(fitness2, fitness1+20);
+      Assert.Greater(fitness2, fitness1 + 20);
 
     }
 
