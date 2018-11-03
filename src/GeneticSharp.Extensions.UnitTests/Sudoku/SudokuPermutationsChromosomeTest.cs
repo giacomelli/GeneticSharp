58302ddda0f1545a5abb0c7e6e3625e97f174b75
giacomelli <giacomelli@gmail.comi>
2018-11-03 21:06:11 -0200

    Splitting Sudoku unit tests into separate classes and improving code coverage

---
+++ src/GeneticSharp.Extensions.UnitTests/Sudoku/SudokuPermutationsChromosomeTest.cs
@@ -0,0 +1,34 @@
+﻿using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Extensions.Sudoku;
+using NUnit.Framework;
+
+namespace GeneticSharp.Extensions.UnitTests.Sudoku
+{
+    [TestFixture]
+    [Category("Extensions")]
+    public class SudokuPermutationsChromosomeTest
+    {
+        [Test]
+        public void Constructor_NoArgs_Length9()
+        {
+            var target = new SudokuPermutationsChromosome();
+            Assert.AreEqual(9, target.Length);
+
+            var genes = target.GetGenes();
+            Assert.AreEqual(9, genes.Length);
+        }
+
+        /// <summary>
+        /// The permutation chromosome should always solve the sudoku in a reasonable time with 1000 chromosomes
+        /// </summary>
+        [Test]
+        public void Evolve_SimpleSudokuPermutationsChromosome_Solved()
+        {
+            var sudoku = SudokuTestHelper.CreateBoard();
+
+            IChromosome chromosome = new SudokuPermutationsChromosome(sudoku);
+            var fitness = SudokuTestHelper.Eval(chromosome, sudoku, 1000, 0, 50);
+            Assert.AreEqual(fitness, 0);
+        }
+    }
+}
