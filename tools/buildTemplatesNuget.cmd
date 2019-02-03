916cacd9706b1117ea6fcf8aaff1d0315f2bea44
giacomelli <giacomelli@gmail.com>
2019-02-03 09:23:45 -0200

    AppVeyor does not support Console.Clear()

--- tools/buildTemplatesNuget.cmd
+++ tools/buildTemplatesNuget.cmd
@@ -5,15 +5,13 @@ dotnet new -i GeneticSharp.Templates.1.0.0.nupkg
 
 echo GeneticSharpConsoleApp
 dotnet new GeneticSharpConsoleApp -n ConsoleApp -o ConsoleApp
-rem We don't run it, because this templates throw an exception asking to programmer to implement the Chromomosome representation.
 dotnet build ConsoleApp/ConsoleApp.csproj
 
 echo GeneticSharpTspConsoleApp
 dotnet new GeneticSharpTspConsoleApp -n TspConsoleApp -o TspConsoleApp
-dotnet run --project TspConsoleApp/TspConsoleApp.csproj
+dotnet build TspConsoleApp/TspConsoleApp.csproj
 
 echo GeneticSharpTspUnity3d
 dotnet new GeneticSharpTspUnity3d -n TspUnity3d -o TspUnity3d
-rem We don't build this template, because it needs Unity3d installed.
 
-cd -
+cd ..\..
