69a67c6f4bfa7868b941fd90e54da352e86c63c5
giacomelli <giacomelli@gmail.com>
2019-02-03 09:20:29 -0200

    Using --project argument

--- tools/buildTemplatesNuget.cmd
+++ tools/buildTemplatesNuget.cmd
@@ -3,16 +3,16 @@ cd .\src\Templates
 nuget pack -Version 1.0.0
 dotnet new -i GeneticSharp.Templates.1.0.0.nupkg
 
-rem GeneticSharpConsoleApp
+echo GeneticSharpConsoleApp
 dotnet new GeneticSharpConsoleApp -n ConsoleApp -o ConsoleApp
 rem We don't run it, because this templates throw an exception asking to programmer to implement the Chromomosome representation.
 dotnet build ConsoleApp/ConsoleApp.csproj
 
-rem GeneticSharpTspConsoleApp
+echo GeneticSharpTspConsoleApp
 dotnet new GeneticSharpTspConsoleApp -n TspConsoleApp -o TspConsoleApp
-dotnet run TspConsoleApp/TspConsoleApp.csproj
+dotnet run --project TspConsoleApp/TspConsoleApp.csproj
 
-rem GeneticSharpTspUnity3d
+echo GeneticSharpTspUnity3d
 dotnet new GeneticSharpTspUnity3d -n TspUnity3d -o TspUnity3d
 rem We don't build this template, because it needs Unity3d installed.
 
