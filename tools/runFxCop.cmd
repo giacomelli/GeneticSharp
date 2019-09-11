90e7f86f18bc0e191129715938925a483e406c6d
Diego giacomelli <giacomelli@gmail.com>
2019-09-11 08:06:01 -0300

    Added build using Cake recipe

--- tools/runFxCop.cmd
+++
@@ -1,9 +0,0 @@
-@echo ------------------------------------------
-@echo Running FxCop...
-@echo ------------------------------------------
-@"C:\Program Files (x86)\Microsoft Fxcop 10.0\FxCopCmd.exe" /project:"..\SRC\GeneticSharp.FxCop" /out:fxcop-report.xml
-@echo fxcop-report.xml file created.
-
-@echo Done!
-@pause
-@exit
