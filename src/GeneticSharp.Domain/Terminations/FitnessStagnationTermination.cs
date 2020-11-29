978460e4094fce30da0f97d236e14d2e2247fb66
Jean-Sylvain Boige <jsboige@gmail.com>
2020-11-29 12:35:20 +0100

    Updated FitnessStagnation to use ratio

--- src/GeneticSharp.Domain/Terminations/FitnessStagnationTermination.cs
+++ src/GeneticSharp.Domain/Terminations/FitnessStagnationTermination.cs
@@ -12,8 +12,9 @@ namespace GeneticSharp.Domain.Terminations
     public class FitnessStagnationTermination : TerminationBase
     {
         #region Fields
-        private double m_lastFitness;
+        private double m_targetFitness;
         private int m_stagnantGenerationsCount;
+        
         #endregion
 
         #region Constructors
@@ -42,6 +43,15 @@ namespace GeneticSharp.Domain.Terminations
         /// Gets or sets the expected stagnant generations number to reach the termination.
         /// </summary>
         public int ExpectedStagnantGenerationsNumber { get; set; }
+
+
+
+        /// <summary>
+        /// max ratio between last fitness and stagnant fitness
+        /// </summary>
+        public double StagnationRatio { get; set; }
+
+
         #endregion
 
         #region Methods
@@ -54,18 +64,24 @@ namespace GeneticSharp.Domain.Terminations
         {
             var bestFitness = geneticAlgorithm.BestChromosome.Fitness.Value;
 
-            if (m_lastFitness == bestFitness)
+            
+            if (m_targetFitness >= bestFitness)
             {
                 m_stagnantGenerationsCount++;
             }
             else
             {
                 m_stagnantGenerationsCount = 1;
+                m_targetFitness = bestFitness * StagnationRatio;
             }
 
-            m_lastFitness = bestFitness;
-
-            return m_stagnantGenerationsCount >= ExpectedStagnantGenerationsNumber;
+            if (m_stagnantGenerationsCount >= ExpectedStagnantGenerationsNumber)
+            {
+                m_targetFitness = 0;
+                m_stagnantGenerationsCount = 1;
+                return true;
+            }
+            return false;
         }
         #endregion
     }
