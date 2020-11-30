ac43e3b498c03cf16f69b31d0094f0edf4877f9e
Jean-Sylvain Boige <jsboige@gmail.com>
2020-11-30 02:47:59 +0100

    Upgraded TspFitness and corresponding unit tests

--- src/GeneticSharp.Extensions.UnitTests/Tsp/TspFitnessTest.cs
+++ src/GeneticSharp.Extensions.UnitTests/Tsp/TspFitnessTest.cs
@@ -1,5 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using GeneticSharp.Domain.Chromosomes;
 using GeneticSharp.Extensions.Tsp;
+using GeneticSharp.Infrastructure.Framework.Collections;
 using NUnit.Framework;
 
 namespace GeneticSharp.Extensions.UnitTests.Tsp
@@ -18,24 +23,109 @@ namespace GeneticSharp.Extensions.UnitTests.Tsp
             Assert.AreEqual("maxY", actual.ParamName);
         }
 
-        [Test()]
-        public void Evaluate_ChromosomeWithLowerCities_FitnessDividedByDiff()
-        {
-            var target = new TspFitness(10, 0, 10, 0, 10);
-            var chromosome = new TspChromosome(9);
 
-            var actual = target.Evaluate(chromosome);
-            Assert.AreNotEqual(0, actual);
+        [Test()]
+        public void Evaluate_Manychromosomes_Cached_Faster()
+        {
+            // Note that with higher numbers the situation eventually reverses and the memory impediment must be detrimental
+            var cityNbs = new[] { 5, 10, 100, 1000 };
+
+            foreach (var cityNb in cityNbs)
+            {
+                var chromosomes = Enumerable.Range(0, 2000).Select(i => new TspChromosome(cityNb).Initialized());
+
+                var fitness = new TspFitness(cityNb, -cityNb, cityNb, -cityNb, cityNb);
+                fitness.Cached = false;
+
+                var sw = Stopwatch.StartNew();
+                chromosomes.Each(tspChromosome => fitness.Evaluate(tspChromosome));
+                var durationNonOptimised = sw.Elapsed;
+
+                fitness.Cached = true;
+                var cachedDistances = fitness.CityDistances;
+                sw.Restart();
+                chromosomes.Each(tspChromosome => fitness.Evaluate(tspChromosome));
+                var durationOptimised = sw.Elapsed;
+
+                Assert.Greater(durationNonOptimised, durationOptimised);
+                
+            }
+
+        }
+
+
+        [Test()]
+        public void Evaluate_DefaultChromosome_FitnessDividedByMissingCities()
+        {
+
+            var cityNbs = new[] {5, 10, 100, 1000, 100000};
+
+            foreach (var cityNb in cityNbs)
+            {
+                var target = new TspFitness(cityNb, -cityNb, cityNb, -cityNb, cityNb);
+                var chromosome = new TspChromosome(cityNb).Initialized();
+
+                //A random chromosome should have a strictly positive fitness
+                var initial = target.Evaluate(chromosome);
+                Assert.Greater(initial, 0);
+
+                chromosome = new TspChromosome(cityNb-1).Initialized();
+
+                var divided = target.Evaluate(chromosome);
+                Assert.Greater(divided, 0);
+
+                Assert.Less(divided, initial);
+
+                chromosome = new TspChromosome(cityNb - 2).Initialized();
+
+                var dividedBis = target.Evaluate(chromosome);
+                Assert.Greater(dividedBis, 0);
+
+                Assert.Less(dividedBis, divided);
+
+                chromosome = new TspChromosome(cityNb/2).Initialized();
+
+                var scaledAndDivided = target.Evaluate(chromosome);
+
+                Assert.Greater(scaledAndDivided, 0);
+
+                Assert.Less(scaledAndDivided, divided);
+            }
+
         }
 
         [Test()]
-        public void Evaluate_FitnessLowerThanZero_Zero()
+        public void Evaluate_RandomChromosome_TightBounds()
+        {
+            var cityNbs = new[] { 5, 10, 20, 50, 100, 200, 1000, 5000, 10000, 100000 };
+            var fitnesses = new List<double>(cityNbs.Length);
+            foreach (var cityNb in cityNbs)
+            {
+                var target = new TspFitness(cityNb, -cityNb, cityNb, -cityNb, cityNb);
+                var chromosome = new TspChromosome(cityNb).Initialized();
+
+                fitnesses.Add(target.Evaluate(chromosome));
+            }
+
+            fitnesses.Each(d =>
+            {
+                Assert.Greater(d, 0.3);
+                Assert.Less(d, 0.7);
+            });
+
+            var meanDefaultFitness = fitnesses.Sum() / fitnesses.Count;
+            Assert.Greater(meanDefaultFitness,0.45);
+            Assert.Less(meanDefaultFitness, 0.55);
+
+        }
+
+        [Test()]
+        public void Evaluate_FitnessGreaterThanZero()
         {
             var target = new TspFitness(10, 0, 10000000, 0, 10000000);
-            var chromosome = new TspChromosome(10);
-            chromosome.InitializeGenes();
+            var chromosome = new TspChromosome(10).Initialized();
             var actual = target.Evaluate(chromosome);
-            Assert.AreEqual(0, actual);
+            Assert.Greater(actual,0);
         }
     }
 }
--- src/GeneticSharp.Extensions/Tsp/TspFitness.cs
+++ src/GeneticSharp.Extensions/Tsp/TspFitness.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Globalization;
 using System.Linq;
 using GeneticSharp.Domain.Chromosomes;
@@ -19,6 +20,17 @@ namespace GeneticSharp.Extensions.Tsp
     /// </summary>
     public class TspFitness : IFitness
     {
+        #region Private fields
+
+        private double? mMinDistanceApprox;
+        private double? mMaxDistanceApprox;
+        private (TspCity, TspCity)? mBoundingBox;
+        private List<List<double>> _cityDistances;
+        private object mLock = new object();
+
+        #endregion
+
+
         #region Constructors
         /// <summary>
         /// Initializes a new instance of the <see cref="GeneticSharp.Extensions.Tsp.TspFitness"/> class.
@@ -35,6 +47,7 @@ namespace GeneticSharp.Extensions.Tsp
             MaxX = maxX;
             MinY = minY;
             MaxY = maxY;
+
 
             if (maxX >= int.MaxValue)
             {
@@ -84,53 +97,175 @@ namespace GeneticSharp.Extensions.Tsp
         /// </summary>
         /// <value>The max y.</value>
         public int MaxY { get; private set; }
+
+        /// <summary>
+        /// Corresponding to edge case with cities aligned 
+        /// </summary>
+        public double MinDistanceApprox
+        {
+            get
+            {
+                if (!mMinDistanceApprox.HasValue)
+                {
+                    lock (mLock)
+                    {
+                        mMinDistanceApprox = 2 * CalcDistanceTwoCities(BoundingBox.Value.Item1, BoundingBox.Value.Item2);
+                    }
+                }
+
+                return mMinDistanceApprox.Value;
+            }
+        }
+
+        /// <summary>
+        /// Corresponding to edge case with half cities in opposite corners of bounding box
+        /// </summary>
+        public double MaxDistanceApprox
+        {
+            get
+            {
+                if (!mMaxDistanceApprox.HasValue)
+                {
+                    lock (mLock)
+                    {
+                        mMaxDistanceApprox = (Cities.Count) * CalcDistanceTwoCities(BoundingBox.Value.Item1, BoundingBox.Value.Item2);
+                    }
+                    
+                }
+
+                return mMaxDistanceApprox.Value;
+            }
+
+        }
+
+        /// <summary>
+        /// The smallest rectangle that comprises all cities
+        /// </summary>
+        public (TspCity, TspCity)? BoundingBox
+        {
+            get
+            {
+                if (!mBoundingBox.HasValue)
+                {
+                    lock (mLock)
+                    {
+                        mBoundingBox = GetBoundingBox();
+                    }
+                }
+                return mBoundingBox;
+            }
+        }
+
+        /// <summary>
+        /// Defines whether the fitness instance should keep city distances in a cached dictionary for easier retrieval
+        /// </summary>
+        public bool Cached { get; set; } = true;
+
+        /// <summary>
+        /// The lazy-loaded dictionary of all city distances
+        /// </summary>
+        public List<List<double>> CityDistances
+        {
+            get
+            {
+                if (_cityDistances == null)
+                {
+                    lock (mLock)
+                    {
+                        if (_cityDistances == null)
+                        {
+                            _cityDistances = BuildPairDistances();
+                        }
+                    }
+                }
+                return _cityDistances;
+
+            }
+        }
+
         #endregion
+
 
         #region IFitness implementation
         /// <summary>
-        /// Performs the evaluation against the specified chromosome.
+        /// Performs the evaluation against the specified chromosome. Assumes gene having int32 city indices.
+        /// Computes the distance of the corresponding city path.
+        /// Then compares the distance to known minimum bound (with fitness 1) and maximum bound (with fitness 0)
+        /// Normalises the result with random path at about fitness 0.5 consistantly among city numbers
+        /// If the tour is missing some cities, the resulting fitness is divided by 1+the number of missing cities
         /// </summary>
-        /// <param name="chromosome">The chromosome to be evaluated.</param>
+        /// <param name="chromosome">The chromosome to be evaluated, with integer genes for city indices.</param>
         /// <returns>The fitness of the chromosome.</returns>
         public double Evaluate(IChromosome chromosome)
         {
-            var genes = chromosome.GetGenes();
-            var distanceSum = 0.0;
-            var lastCityIndex = Convert.ToInt32(genes[0].Value, CultureInfo.InvariantCulture);
-            var citiesIndexes = new List<int>
-            {
-                lastCityIndex
-            };
 
-            for (int i = 0, genesLength = genes.Length; i < genesLength; i++)
-            {
-                var currentCityIndex = Convert.ToInt32(genes[i].Value, CultureInfo.InvariantCulture);
-                distanceSum += CalcDistanceTwoCities(Cities[currentCityIndex], Cities[lastCityIndex]);
-                lastCityIndex = currentCityIndex;
+            var distanceSum = ComputeDistance(chromosome, out var citiesIndexes);
 
-                citiesIndexes.Add(lastCityIndex);
-            }
-
-            distanceSum += CalcDistanceTwoCities(Cities[citiesIndexes.Last()], Cities[citiesIndexes.First()]);
-
-            var fitness = 1.0 - (distanceSum / (Cities.Count * 1000.0));
+            //Calibrated to yield about 0.5 for random path, and close to 1 when distanceSum closes on approx MinDistanceApprox, and 0 when closing on MaxDistanceApprox
+            var fitness = 1 - (distanceSum - MinDistanceApprox) /((double) MaxDistanceApprox) ;   // 1.0 - distanceSum / MaxDistanceApprox;
 
             ((TspChromosome)chromosome).Distance = distanceSum;
 
-            // There is repeated cities on the indexes?
+            if (fitness < 0)
+            {
+                //Worst than worst case
+                fitness = 0;
+            }
+
+            // There is repeated cities, or missing cities on the indexes?
             var diff = Cities.Count - citiesIndexes.Distinct().Count();
 
             if (diff > 0)
             {
-                fitness /= diff;
+                fitness /= (diff+1);
             }
-
-            if (fitness < 0)
-            {
-                fitness = 0;
-            }
+            
 
             return fitness;
+        }
+
+        #endregion
+
+
+        #region Methods
+
+      
+
+        /// <summary>
+        /// Computes the distance of tourning all input cities from a chromosome with integer index genes in a closed circuit 
+        /// </summary>
+        public double ComputeDistance(IChromosome chromosome, out List<int> cityIndices)
+        {
+            cityIndices = chromosome.GetGenes().Select(g=>(int?) g.Value ?? 0).ToList();
+            return ComputeDistance(cityIndices);
+        }
+
+        /// <summary>
+        /// Computes the distance of tourning all input city indices in a closed circuit 
+        /// </summary>
+        public double ComputeDistance(IList<int> cityIndices)
+        {
+            
+            var distanceSum = 0.0;
+            var lastCityIndex = cityIndices[cityIndices.Count -1];
+
+            for (int i = 0;  i < cityIndices.Count; i++)
+            {
+                var currentCityIndex = cityIndices[i];
+
+                if (Cached)
+                {
+                    distanceSum += CityDistances[lastCityIndex][currentCityIndex];
+                }
+                else
+                {
+                    distanceSum += CalcDistanceTwoCities(Cities[currentCityIndex], Cities[lastCityIndex]);
+                }
+                lastCityIndex = currentCityIndex;
+
+
+            }
+            return distanceSum;
         }
 
         /// <summary>
@@ -139,10 +274,47 @@ namespace GeneticSharp.Extensions.Tsp
         /// <returns>The distance two cities.</returns>
         /// <param name="one">City one.</param>
         /// <param name="two">City two.</param>
-        private static double CalcDistanceTwoCities(TspCity one, TspCity two)
+        public static double CalcDistanceTwoCities(TspCity one, TspCity two)
         {
             return Math.Sqrt(Math.Pow(two.X - one.X, 2) + Math.Pow(two.Y - one.Y, 2));
         }
+
+        private (TspCity, TspCity) GetBoundingBox()
+        {
+            var xMin = Cities.Min(c => c.X);
+            var xMax = Cities.Max(c => c.X);
+            var yMin = Cities.Min(c => c.Y);
+            var yMax = Cities.Min(c => c.Y);
+            return (new TspCity(xMin, yMin), new TspCity(xMax, yMax));
+        }
+
+        /// <summary>
+        /// Builds a double dictionary structure with all city distance pairs
+        /// </summary>
+        private List<List<double>> BuildPairDistances()
+        {
+            //Note: The pairs are computed twice, could be optimized
+            var toReturn = new List<List<double>>(Cities.Count);
+            for (int i = 0; i < Cities.Count; i++)
+            {
+                var distFromI = new List<double>(Cities.Count);
+                for (int j = 0; j < Cities.Count; j++)
+                {
+                    if (i==j)
+                    {
+                        distFromI.Add(0);
+                    }
+                    else
+                    {
+                        distFromI.Add(TspFitness.CalcDistanceTwoCities(Cities[i], Cities[j]));
+                    }
+
+                }
+                toReturn.Add(distFromI);
+            }
+            return toReturn;
+        }
+
         #endregion
     }
 }
