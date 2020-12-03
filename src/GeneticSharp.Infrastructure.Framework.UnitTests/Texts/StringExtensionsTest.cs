5a9985ff9af8066772c926f6be6b59e8dfe347a0
Jean-Sylvain Boige <jsboige@gmail.com>
2020-12-03 17:27:50 +0100

    optimizing and covering string distances

--- src/GeneticSharp.Infrastructure.Framework.UnitTests/Texts/StringExtensionsTest.cs
+++ src/GeneticSharp.Infrastructure.Framework.UnitTests/Texts/StringExtensionsTest.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using GeneticSharp.Infrastructure.Framework.Collections;
 using GeneticSharp.Infrastructure.Framework.Texts;
 using NUnit.Framework;
 
@@ -18,5 +21,80 @@ namespace GeneticSharp.Infrastructure.Framework.UnitTests.Texts
         {
             Assert.AreEqual("A1b2", "A{0}b{1}".With(1, 2));
         }
+
+
+        [Test()]
+        public void HammingDistance_EqualLength_CorrectValue()
+        {
+            Assert.Catch(() => "abc".HammingDistance("ab"));
+
+            var tests = new List<(string s1, string s2, int distance)>();
+            tests.Add(("karolin", "kathrin", 3));
+            tests.Add(("kathrin", "kerstin", 4));
+            tests.Add(("1011101", "1001001", 2));
+            tests.Add(("2173896", "2233796", 3));
+
+            tests.Each(tuple => Assert.AreEqual(tuple.distance, tuple.s1.LevenshteinDistance(tuple.s2)));
+
+        }
+
+
+
+        [Test()]
+        public void LevenshteinDistance_CorrectValue()
+        {
+            var tests = new List<(string s1, string s2, int distance)>();
+            tests.Add(("niche", "chien", 4));
+            tests.Add(("GeneticSharp", "SharpGenetic", 10));
+            tests.Add(("vladimir", "levenshtein", 9));
+
+            tests.Each(tuple => Assert.AreEqual(tuple.distance, tuple.s1.LevenshteinDistance(tuple.s2)));
+
+          
+        }
+
+        [Test()]
+        public void DamerauLevenshteinDistance_CorrectValue()
+        {
+
+            var tests = new List<(string s1, string s2, int distance)>();
+            tests.Add(("climax", "volmax", 3));//value where true DamerauLevenshtein should be 2
+            tests.Add(("Ram", "Rom", 1));
+            tests.Add(("jellyifhs", "jellyfish", 3));//value where true DamerauLevenshtein should be 2
+
+            tests.Each(tuple => Assert.AreEqual(tuple.distance, tuple.s1.LevenshteinDistance(tuple.s2)));
+
+        }
+
+
+        [Test()]
+        public void DamerauLevenshteinDistanceBoundedVersion_Faster()
+        {
+            
+            var str1 = "abcdefghijklmnopqrstuvwxyzjellyfishabcdefghijklmnopqrstuvwxyz";
+            var str2 = "efghijklmnopqrstuvwxyzjellyifshabcdefghijklmnopqrstuvwxyzabcd";
+
+            int d1 = 0, d2 = 0;
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < 10000; i++)
+            {
+                d1 = str1.DamerauLevenshteinDistance(str2);
+            }
+            var firstDuration = sw.Elapsed;
+            sw.Restart();
+            for (int i = 0; i < 10000; i++)
+            {
+                d2 = str1.DamerauLevenshteinDistance(str2, 9);
+            }
+            var fasterVersionDuration = sw.Elapsed;
+            Assert.AreEqual(d1,d2);
+
+            
+            Assert.Greater(firstDuration.Ticks, fasterVersionDuration.Ticks * 2);
+
+        }
+
+
+
     }
 }
--- src/GeneticSharp.Infrastructure.Framework/Texts/StringExtensions.cs
+++ src/GeneticSharp.Infrastructure.Framework/Texts/StringExtensions.cs
@@ -39,7 +39,7 @@ namespace GeneticSharp.Infrastructure.Framework.Texts
 
 
         /// <summary>
-        /// Computes the hamming distance between 2 strings, that is, the number of distinct chars
+        /// Computes the hamming distance between 2 strings of equal lengths, that is, the number of distinct chars
         /// </summary>
         public static int HammingDistance(this string s, string t)
         {
@@ -58,88 +58,300 @@ namespace GeneticSharp.Infrastructure.Framework.Texts
 
 
         /// <summary>
-        /// Computes the Levenshtein distance between two strings.
+        /// Computes and returns the Levenshtein edit distance between two strings, i.e. the
+        /// number of insertion, deletion, and sustitution edits required to transform one
+        /// string to the other. This value will be >= 0, where 0 indicates identical strings.
+        /// Comparisons are case sensitive, so for example, "Fred" and "fred" will have a 
+        /// distance of 1.
+        /// http://blog.softwx.net/2014/12/optimizing-levenshtein-algorithm-in-c.html
         /// </summary>
+        /// <remarks>See http://en.wikipedia.org/wiki/Levenshtein_distance
+        /// This is based on Sten Hjelmqvist's "Fast, memory efficient" algorithm, described
+        /// at http://www.codeproject.com/Articles/13525/Fast-memory-efficient-Levenshtein-algorithm, 
+        /// with some additional optimizations.
+        /// </remarks>
+        /// <param name="s">String being compared for distance.</param>
+        /// <param name="t">String being compared against other string.</param>
+        /// <returns>int edit distance, >= 0 representing the number of edits required to transform one string to the other.</returns>
         public static int LevenshteinDistance(this string s, string t)
         {
-            int n = s.Length;
-            int m = t.Length;
-            int[,] d = new int[n + 1, m + 1];
+            if (String.IsNullOrEmpty(s)) return (t ?? "").Length;
+            if (String.IsNullOrEmpty(t)) return s.Length;
 
-            // Step 1
-            if (n == 0)
+            // if strings of different lengths, ensure shorter string is in s. This can result in a little
+            // faster speed by spending more time spinning just the inner loop during the main processing.
+            if (s.Length > t.Length)
             {
-                return m;
+                var temp = s; s = t; t = temp; // swap s and t
             }
+            int sLen = s.Length; // this is also the minimun length of the two strings
+            int tLen = t.Length;
 
-            if (m == 0)
-            {
-                return n;
+            // suffix common to both strings can be ignored
+            while ((sLen > 0) && (s[sLen - 1] == t[tLen - 1])) { sLen--; tLen--; }
+
+            int start = 0;
+            if ((s[0] == t[0]) || (sLen == 0))
+            { // if there's a shared prefix, or all s matches t's suffix
+              // prefix common to both strings can be ignored
+                while ((start < sLen) && (s[start] == t[start])) start++;
+                sLen -= start; // length of the part excluding common prefix and suffix
+                tLen -= start;
+
+                // if all of shorter string matches prefix and/or suffix of longer string, then
+                // edit distance is just the delete of additional characters present in longer string
+                if (sLen == 0) return tLen;
+
+                t = t.Substring(start, tLen); // faster than t[start+j] in inner loop below
             }
+            var v0 = new int[tLen];
+            for (int j = 0; j < tLen; j++) v0[j] = j + 1;
 
-            // Step 2
-            for (int i = 0; i <= n; d[i, 0] = i++)
+            int current = 0;
+            for (int i = 0; i < sLen; i++)
             {
-            }
-
-            for (int j = 0; j <= m; d[0, j] = j++)
-            {
-            }
-
-            // Step 3
-            for (int i = 1; i <= n; i++)
-            {
-                //Step 4
-                for (int j = 1; j <= m; j++)
+                char sChar = s[start + i];
+                int left = current = i;
+                for (int j = 0; j < tLen; j++)
                 {
-                    // Step 5
-                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
-
-                    // Step 6
-                    d[i, j] = Math.Min(
-                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
-                        d[i - 1, j - 1] + cost);
+                    int above = current;
+                    current = left; // cost on diagonal (substitution)
+                    left = v0[j];
+                    if (sChar != t[j])
+                    {
+                        current++;              // substitution
+                        int insDel = above + 1; // deletion
+                        if (insDel < current) current = insDel;
+                        insDel = left + 1;      // insertion
+                        if (insDel < current) current = insDel;
+                    }
+                    v0[j] = current;
                 }
             }
-            // Step 7
-            return d[n, m];
+            return current;
         }
+
 
         /// <summary>
-        /// Computes the Damerau Levenshtein distance between two strings.
+        ///  Computes and returns the Damerau-Levenshtein edit distance between two strings, 
+        /// i.e. the number of insertion, deletion, sustitution, and transposition edits
+        /// required to transform one string to the other. This value will be >= 0, where 0
+        /// indicates identical strings. Comparisons are case sensitive, so for example, 
+        /// "Fred" and "fred" will have a distance of 1. This algorithm is basically the
+        /// Levenshtein algorithm with a modification that considers transposition of two
+        /// adjacent characters as a single edit.
+        /// http://blog.softwx.net/2015/01/optimizing-damerau-levenshtein_15.html
         /// </summary>
+        /// <remarks>See http://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance
+        /// Note that this is based on Sten Hjelmqvist's "Fast, memory efficient" algorithm, described
+        /// at http://www.codeproject.com/Articles/13525/Fast-memory-efficient-Levenshtein-algorithm.
+        /// This version differs by including some optimizations, and extending it to the Damerau-
+        /// Levenshtein algorithm.
+        /// Note that this is the simpler and faster optimal string alignment (aka restricted edit) distance
+        /// that difers slightly from the classic Damerau-Levenshtein algorithm by imposing the restriction
+        /// that no substring is edited more than once. So for example, "CA" to "ABC" has an edit distance
+        /// of 2 by a complete application of Damerau-Levenshtein, but a distance of 3 by this method that
+        /// uses the optimal string alignment algorithm. See wikipedia article for more detail on this
+        /// distinction.
+        /// </remarks>
+        /// <param name="s">String being compared for distance.</param>
+        /// <param name="t">String being compared against other string.</param>
+        /// <returns>int edit distance, >= 0 representing the number of edits required
+        /// to transform one string to the other.</returns>
         public static int DamerauLevenshteinDistance(this string s, string t)
         {
-            var bounds = new { Height = s.Length + 1, Width = t.Length + 1 };
+            if (String.IsNullOrEmpty(s)) return (t ?? "").Length;
+            if (String.IsNullOrEmpty(t)) return s.Length;
 
-            int[,] matrix = new int[bounds.Height, bounds.Width];
-
-            for (int height = 0; height < bounds.Height; height++) { matrix[height, 0] = height; };
-            for (int width = 0; width < bounds.Width; width++) { matrix[0, width] = width; };
-
-            for (int height = 1; height < bounds.Height; height++)
+            // if strings of different lengths, ensure shorter string is in s. This can result in a little
+            // faster speed by spending more time spinning just the inner loop during the main processing.
+            if (s.Length > t.Length)
             {
-                for (int width = 1; width < bounds.Width; width++)
-                {
-                    int cost = (s[height - 1] == t[width - 1]) ? 0 : 1;
-                    int insertion = matrix[height, width - 1] + 1;
-                    int deletion = matrix[height - 1, width] + 1;
-                    int substitution = matrix[height - 1, width - 1] + cost;
+                var temp = s; s = t; t = temp; // swap s and t
+            }
+            int sLen = s.Length; // this is also the minimun length of the two strings
+            int tLen = t.Length;
 
-                    int distance = Math.Min(insertion, Math.Min(deletion, substitution));
+            // suffix common to both strings can be ignored
+            while ((sLen > 0) && (s[sLen - 1] == t[tLen - 1])) { sLen--; tLen--; }
 
-                    if (height > 1 && width > 1 && s[height - 1] == t[width - 2] && s[height - 2] == t[width - 1])
-                    {
-                        distance = Math.Min(distance, matrix[height - 2, width - 2] + cost);
-                    }
+            int start = 0;
+            if ((s[0] == t[0]) || (sLen == 0))
+            { // if there's a shared prefix, or all s matches t's suffix
+              // prefix common to both strings can be ignored
+                while ((start < sLen) && (s[start] == t[start])) start++;
+                sLen -= start; // length of the part excluding common prefix and suffix
+                tLen -= start;
 
-                    matrix[height, width] = distance;
-                }
+                // if all of shorter string matches prefix and/or suffix of longer string, then
+                // edit distance is just the delete of additional characters present in longer string
+                if (sLen == 0) return tLen;
+
+                t = t.Substring(start, tLen); // faster than t[start+j] in inner loop below
             }
 
-            return matrix[bounds.Height - 1, bounds.Width - 1];
+            var v0 = new int[tLen];
+            var v2 = new int[tLen]; // stores one level further back (offset by +1 position)
+            for (int j = 0; j < tLen; j++) v0[j] = j + 1;
+
+            char sChar = s[0];
+            int current = 0;
+            for (int i = 0; i < sLen; i++)
+            {
+                char prevsChar = sChar;
+                sChar = s[start + i];
+                char tChar = t[0];
+                int left = i;
+                current = i + 1;
+                int nextTransCost = 0;
+                for (int j = 0; j < tLen; j++)
+                {
+                    int above = current;
+                    int thisTransCost = nextTransCost;
+                    nextTransCost = v2[j];
+                    v2[j] = current = left; // cost of diagonal (substitution)
+                    left = v0[j];    // left now equals current cost (which will be diagonal at next iteration)
+                    char prevtChar = tChar;
+                    tChar = t[j];
+                    if (sChar != tChar)
+                    {
+                        if (left < current) current = left;   // insertion
+                        if (above < current) current = above; // deletion
+                        current++;
+                        if ((i != 0) && (j != 0)
+                            && (sChar == prevtChar)
+                            && (prevsChar == tChar))
+                        {
+                            thisTransCost++;
+                            if (thisTransCost < current) current = thisTransCost; // transposition
+                        }
+                    }
+                    v0[j] = current;
+                }
+            }
+            return current;
+        }
+        ///<summary>
+        /// Computes and returns the Damerau-Levenshtein edit distance between two strings, 
+        /// i.e. the number of insertion, deletion, sustitution, and transposition edits
+        /// required to transform one string to the other. This value will be >= 0, where 0
+        /// indicates identical strings. Comparisons are case sensitive, so for example, 
+        /// "Fred" and "fred" will have a distance of 1. This algorithm is basically the
+        /// Levenshtein algorithm with a modification that considers transposition of two
+        /// adjacent characters as a single edit.
+        /// This version takes an additional parameter that lets you specify a maximum distance. Specifying a maximum distance allows important optimizations.
+        /// http://blog.softwx.net/2015/01/optimizing-damerau-levenshtein_15.html
+        /// </summary>
+        /// <remarks>See http://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance
+        /// Note that this is based on Sten Hjelmqvist's "Fast, memory efficient" algorithm, described
+        /// at http://www.codeproject.com/Articles/13525/Fast-memory-efficient-Levenshtein-algorithm.
+        /// This version differs by including some optimizations, and extending it to the Damerau-
+        /// Levenshtein algorithm.
+        /// Note that this is the simpler and faster optimal string alignment (aka restricted edit) distance
+        /// that difers slightly from the classic Damerau-Levenshtein algorithm by imposing the restriction
+        /// that no substring is edited more than once. So for example, "CA" to "ABC" has an edit distance
+        /// of 2 by a complete application of Damerau-Levenshtein, but a distance of 3 by this method that
+        /// uses the optimal string alignment algorithm. See wikipedia article for more detail on this
+        /// distinction.
+        /// </remarks>
+        /// <param name="s">String being compared for distance.</param>
+        /// <param name="t">String being compared against other string.</param>
+        /// <param name="maxDistance">The maximum edit distance of interest.</param>
+        /// <returns>int edit distance, >= 0 representing the number of edits required
+        /// to transform one string to the other, or -1 if the distance is greater than the specified maxDistance.</returns>
+        public static int DamerauLevenshteinDistance(this string s, string t, int maxDistance)
+        {
+            if (String.IsNullOrEmpty(s)) return ((t ?? "").Length <= maxDistance) ? (t ?? "").Length : -1;
+            if (String.IsNullOrEmpty(t)) return (s.Length <= maxDistance) ? s.Length : -1;
+
+            // if strings of different lengths, ensure shorter string is in s. This can result in a little
+            // faster speed by spending more time spinning just the inner loop during the main processing.
+            if (s.Length > t.Length)
+            {
+                var temp = s; s = t; t = temp; // swap s and t
+            }
+            int sLen = s.Length; // this is also the minimun length of the two strings
+            int tLen = t.Length;
+
+            // suffix common to both strings can be ignored
+            while ((sLen > 0) && (s[sLen - 1] == t[tLen - 1])) { sLen--; tLen--; }
+
+            int start = 0;
+            if ((s[0] == t[0]) || (sLen == 0))
+            { // if there's a shared prefix, or all s matches t's suffix
+              // prefix common to both strings can be ignored
+                while ((start < sLen) && (s[start] == t[start])) start++;
+                sLen -= start; // length of the part excluding common prefix and suffix
+                tLen -= start;
+
+                // if all of shorter string matches prefix and/or suffix of longer string, then
+                // edit distance is just the delete of additional characters present in longer string
+                if (sLen == 0) return (tLen <= maxDistance) ? tLen : -1;
+
+                t = t.Substring(start, tLen); // faster than t[start+j] in inner loop below
+            }
+            int lenDiff = tLen - sLen;
+            if ((maxDistance < 0) || (maxDistance > tLen))
+            {
+                maxDistance = tLen;
+            }
+            else if (lenDiff > maxDistance) return -1;
+
+            var v0 = new int[tLen];
+            var v2 = new int[tLen]; // stores one level further back (offset by +1 position)
+            int j;
+            for (j = 0; j < maxDistance; j++) v0[j] = j + 1;
+            for (; j < tLen; j++) v0[j] = maxDistance + 1;
+
+            int jStartOffset = maxDistance - (tLen - sLen);
+            bool haveMax = maxDistance < tLen;
+            int jStart = 0;
+            int jEnd = maxDistance;
+            char sChar = s[0];
+            int current = 0;
+            for (int i = 0; i < sLen; i++)
+            {
+                char prevsChar = sChar;
+                sChar = s[start + i];
+                char tChar = t[0];
+                int left = i;
+                current = left + 1;
+                int nextTransCost = 0;
+                // no need to look beyond window of lower right diagonal - maxDistance cells (lower right diag is i - lenDiff)
+                // and the upper left diagonal + maxDistance cells (upper left is i)
+                jStart += (i > jStartOffset) ? 1 : 0;
+                jEnd += (jEnd < tLen) ? 1 : 0;
+                for (j = jStart; j < jEnd; j++)
+                {
+                    int above = current;
+                    int thisTransCost = nextTransCost;
+                    nextTransCost = v2[j];
+                    v2[j] = current = left; // cost of diagonal (substitution)
+                    left = v0[j];    // left now equals current cost (which will be diagonal at next iteration)
+                    char prevtChar = tChar;
+                    tChar = t[j];
+                    if (sChar != tChar)
+                    {
+                        if (left < current) current = left;   // insertion
+                        if (above < current) current = above; // deletion
+                        current++;
+                        if ((i != 0) && (j != 0)
+                            && (sChar == prevtChar)
+                            && (prevsChar == tChar))
+                        {
+                            thisTransCost++;
+                            if (thisTransCost < current) current = thisTransCost; // transposition
+                        }
+                    }
+                    v0[j] = current;
+                }
+                if (haveMax && (v0[i + lenDiff] > maxDistance)) return -1;
+            }
+            return (current <= maxDistance) ? current : -1;
         }
 
-
     }
+
+  
+
 }
