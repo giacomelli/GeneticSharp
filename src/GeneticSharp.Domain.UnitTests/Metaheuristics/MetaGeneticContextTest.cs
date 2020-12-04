10756323e081934366469456ed54eb905d1a78ac
Jean-Sylvain Boige <jsboige@gmail.com>
2020-12-04 13:42:02 +0100

    MetaHeuristicContext Unit test

---
+++ src/GeneticSharp.Domain.UnitTests/Metaheuristics/MetaGeneticContextTest.cs
@@ -0,0 +1,25 @@
+﻿using GeneticSharp.Domain.Metaheuristics;
+using NUnit.Framework;
+
+namespace GeneticSharp.Domain.UnitTests.MetaHeuristics
+{
+    [TestFixture()]
+    [Category("MetaHeuristics")]
+    class MetaGeneticContextTest : MetaHeuristicTestBase
+    {
+
+        [Test()]
+        public void IndividualContext_HoldsCorrectInformation()
+        {
+
+            var generationContext = new MetaHeuristicContext();
+            generationContext.GetOrAdd(("test", 1, MetaHeuristicsStage.All, null, 0), () => 2);
+            var indContext = generationContext.GetIndividual(3);
+            Assert.AreEqual(3, indContext.Index);
+            Assert.AreEqual(2, indContext.GetOrAdd(("test", 1, MetaHeuristicsStage.All, null, 0), ()=>3) );
+
+        }
+
+
+    }
+}
