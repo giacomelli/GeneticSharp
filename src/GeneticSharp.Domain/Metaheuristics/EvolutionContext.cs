ac297c2f3fcc2fbb34e7e4670886104001098ea1
Jean-Sylvain Boige <jsboige@gmail.com>
2020-12-09 20:03:39 +0100

    Added comments for context

--- src/GeneticSharp.Domain/Metaheuristics/EvolutionContext.cs
+++ src/GeneticSharp.Domain/Metaheuristics/EvolutionContext.cs
@@ -6,17 +6,34 @@ using GeneticSharp.Domain.Populations;
 
 namespace GeneticSharp.Domain.Metaheuristics
 {
+    /// <summary>
+    /// The default implementation of the <see cref="IEvolutionContext"/> uses a Concurrent Dictionary for parameters to allow for parallel Operators to consume the same context.
+    /// </summary>
     public class EvolutionContext : IEvolutionContext
     {
 
+        #region Fields
+
+
+        private readonly Dictionary<string, IMetaHeuristicParameter> _paramDefinitions = new Dictionary<string, IMetaHeuristicParameter>();
+
+        #endregion
+
+
+        #region Properties
+
+        /// <inheritdoc />
         public IGeneticAlgorithm GeneticAlgorithm { get; set; }
 
+
+        /// <inheritdoc />
         public IPopulation Population { get; set; }
 
 
-       
+        /// <inheritdoc />
         public int Index { get; set; }
 
+        /// <inheritdoc />
         public EvolutionStage CurrentStage { get; set; }
 
 
@@ -25,25 +42,56 @@ namespace GeneticSharp.Domain.Metaheuristics
         /// </summary>
         public ConcurrentDictionary<(string, int, EvolutionStage, IMetaHeuristic, int), object> Params { get; set; } = new ConcurrentDictionary<(string, int, EvolutionStage, IMetaHeuristic, int), object>();
 
-        private readonly Dictionary<string, IMetaHeuristicParameter> _paramDefinitions = new Dictionary<string, IMetaHeuristicParameter>();
 
+
+        #endregion
+
+        #region Public Methods
+
+        /// <inheritdoc />
         public IEvolutionContext GetIndividual(int index)
         {
             return new IndividualContext(this, index);
         }
 
+
+        /// <inheritdoc />
         public TItemType GetOrAdd<TItemType>((string key, int generation, EvolutionStage stage, IMetaHeuristic heuristic, int individual) contextKey, Func<TItemType> factory)
         {
             var toReturn = (TItemType)Params.GetOrAdd(contextKey, s => (object)factory());
             return toReturn;
         }
 
+        /// <inheritdoc />
         public TItemType GetParam<TItemType>(IMetaHeuristic h, string paramName)
         {
             return GetParamWithContext<TItemType>(h, paramName, this);
         }
 
-        public TItemType GetParamWithContext<TItemType>(IMetaHeuristic h, string paramName, IEvolutionContext ctx)
+
+      
+
+        /// <inheritdoc />
+        public void RegisterParameter(string paramName, IMetaHeuristicParameter param)
+        {
+            _paramDefinitions.Add(paramName, param);
+        }
+
+        /// <inheritdoc />
+        public IMetaHeuristicParameter GetParameterDefinition(string paramName)
+        {
+            if (_paramDefinitions.TryGetValue(paramName, out var paramDef))
+            {
+                return paramDef;
+            }
+            throw new ArgumentException($"parameter {paramName} not found in MetaHeuristic expression chain", nameof(paramName));
+        }
+
+        #endregion
+
+        #region Private methods
+
+        internal TItemType GetParamWithContext<TItemType>(IMetaHeuristic h, string paramName, IEvolutionContext ctx)
         {
             _paramDefinitions.TryGetValue(paramName, out var paramDef);
             if (paramDef == null)
@@ -54,22 +102,9 @@ namespace GeneticSharp.Domain.Metaheuristics
         }
 
 
-        public void RegisterParameter(string key, IMetaHeuristicParameter param)
-        {
-            _paramDefinitions.Add(key, param);
-        }
-
-        public IMetaHeuristicParameter GetParameterDefinition(string key)
-        {
-            if (_paramDefinitions.TryGetValue(key, out var paramDef))
-            {
-                return paramDef;
-            }
-            throw new ArgumentException($"parameter {key} not found in MetaHeuristic expression chain", nameof(key));
-        }
+        #endregion
 
 
-       
 
 
     }
--- src/GeneticSharp.Domain/Metaheuristics/IEvolutionContext.cs
+++ src/GeneticSharp.Domain/Metaheuristics/IEvolutionContext.cs
@@ -5,37 +5,70 @@ using GeneticSharp.Domain.Populations;
 namespace GeneticSharp.Domain.Metaheuristics
 {
 
-   
-
-
-
+    /// <summary>
+    /// The Evolution Context serves the evolution state throughout the generations cycles, with access to the current global and scoped parameters.
+    /// </summary>
     public interface IEvolutionContext
     {
-
-        
+        /// <summary>
+        /// The genetic algorithm being run
+        /// </summary>
         IGeneticAlgorithm GeneticAlgorithm { get; set; }
+
+        /// <summary>
+        /// The current population being evolved
+        /// </summary>
         IPopulation Population { get; set; }
 
+        /// <summary>
+        /// The current individual index among stage population (parents for crossover and offspring for mutations)
+        /// </summary>
         int Index { get; set; }
 
+        /// <summary>
+        /// The current evolution stage among <see cref="EvolutionStage"/>
+        /// </summary>
         EvolutionStage CurrentStage { get; set; }
 
+        /// <summary>
+        /// Creates an individual specific context from a generation specific context
+        /// </summary>
+        /// <param name="index">the individual index to personalize the generation context</param>
+        /// <returns>A context with Index set to the passed index</returns>
         IEvolutionContext GetIndividual(int index);
 
         /// <summary>
         /// Allows storing and retrieving objects in a generation based cache, specific to the heuristics or to be used in any heuristics for the current generation
         /// </summary>
         /// <typeparam name="TItemType">The type of object to store</typeparam>
-        /// <param name="contextKey">the key for the object storage and retrieval</param>
+        /// <param name="contextKey">the paramName for the object storage and retrieval</param>
         /// <param name="factory">the factory to build the object if not found in the cache</param>
         /// <returns></returns>
         TItemType GetOrAdd<TItemType>((string key, int generation, EvolutionStage stage, IMetaHeuristic heuristic, int individual) contextKey, Func<TItemType> factory);
 
+        /// <summary>
+        /// Allows invoking a parameter previously registered to the context by the corresponding parameter name, for instance in an Expression based Metaheuristic operator
+        /// </summary>
+        /// <typeparam name="TItemType">The expected parameter type</typeparam>
+        /// <param name="h">the current metaheuristic (maybe null if not scoped by metaheuristic)</param>
+        /// <param name="paramName">The name of the parameter to invoke</param>
+        /// <returns>The typed value of the named parameter invoked</returns>
         TItemType GetParam<TItemType>(IMetaHeuristic h, string paramName);
 
-        void RegisterParameter(string key, IMetaHeuristicParameter param);
+        /// <summary>
+        /// Allows registering a contextual parameter for further invocation.
+        /// </summary>
+        /// <param name="paramName"></param>
+        /// <param name="param"></param>
+        void RegisterParameter(string paramName, IMetaHeuristicParameter param);
 
-        IMetaHeuristicParameter GetParameterDefinition(string key);
+
+        /// <summary>
+        /// Given the name of a previously registered parameter, returns the corresponding definition
+        /// </summary>
+        /// <param name="paramName">The parameter name</param>
+        /// <returns></returns>
+        IMetaHeuristicParameter GetParameterDefinition(string paramName);
 
     }
 }
