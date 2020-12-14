bfff493fd8a551b1599058da99d0dac7e12ca77a
Jean-Sylvain Boige <jsboige@gmail.com>
2020-12-14 11:53:27 +0100

    Introducing Square based exponentiation

---
+++ src/GeneticSharp.Infrastructure.Framework.UnitTests/Commons/MathTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using GeneticSharp.Domain.Randomizations;
+using GeneticSharp.Infrastructure.Framework.Commons;
+using NUnit.Framework;
+
+namespace GeneticSharp.Infrastructure.Framework.UnitTests.Commons
+{
+    [TestFixture]
+    public class MathTest
+    {
+
+        [Test]
+        public void Compare_IntPow_Pow_Faster()
+        {
+            var ratio = 1.0;
+            var repeatNb = 5;
+            var maxApprox = Math.Pow(10, -10);
+
+            var nbNumbers = (int) 10.IntPow(5);
+            var maxPower = 500;
+            var rnd = RandomizationProvider.Current;
+            var rndNumbers = Enumerable.Range(0, nbNumbers).Select(i => (rnd.GetDouble(0, 1) - 1) * 10).ToList();
+
+            for (int i = 0; i < maxPower; i+=50)
+            {
+                TimeSpan regularElapsed = TimeSpan.Zero;
+                TimeSpan optimizedElapsed = TimeSpan.Zero;
+                for (int r = 0; r < repeatNb; r++)
+                {
+                    var regularResults = new List<double>(rndNumbers.Count);
+                    var sw = Stopwatch.StartNew();
+                    foreach (var rndNumber in rndNumbers)
+                    {
+                        regularResults.Add(Math.Pow(rndNumber, i + 2));
+                    }
+                    regularElapsed += sw.Elapsed;
+                    var optimizedResults = new List<double>(rndNumbers.Count);
+                    sw.Restart();
+                    foreach (var rndNumber in rndNumbers)
+                    {
+                        optimizedResults.Add(rndNumber.IntPow(i + 2));
+                    }
+                    optimizedElapsed += sw.Elapsed;
+                    for (int rndIndex = 0; rndIndex < rndNumbers.Count; rndIndex++)
+                    {
+                        Assert.LessOrEqual(Math.Abs(regularResults[rndIndex] - optimizedResults[rndIndex])/ Math.Max(Math.Abs(regularResults[rndIndex]), double.Epsilon), maxApprox);
+                    }
+                }
+
+                Assert.Greater(TimeSpan.FromTicks((long)(regularElapsed.Ticks * ratio)), optimizedElapsed  , $"failed at i = {i}");
+            }
+           
+        }
+
+    }
+}
--- src/GeneticSharp.Infrastructure.Framework/Commons/Math.cs
+++ src/GeneticSharp.Infrastructure.Framework/Commons/Math.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace GeneticSharp.Infrastructure.Framework.Commons
 {
@@ -7,5 +8,29 @@ namespace GeneticSharp.Infrastructure.Framework.Commons
         public static int PositiveMod(this int k, int n) { return (k %= n) < 0 ? k + n : k; }
 
         public static int PositiveMod<TValue>(this TValue k, int n) => PositiveMod(Convert.ToInt32(k), n);
+
+        /// <summary>
+        /// https://en.wikipedia.org/wiki/Exponentiation_by_squaring
+        /// </summary>
+        /// <param name="num">the number to exponentiate</param>
+        /// <param name="exp">the integer exponent</param>
+        /// <returns></returns>
+        public static double IntPow(this double num, int exp)
+        {
+            double result = 1.0;
+            while (exp > 0)
+            {
+                if (exp % 2 == 1)
+                    result *= num;
+                exp >>= 1;
+                num *= num;
+            }
+
+            return result;
+        }
+
+        public static double IntPow<TValue>(this TValue num, int exp) => Convert.ToDouble(num).IntPow(exp);
+
+
     }
 }
