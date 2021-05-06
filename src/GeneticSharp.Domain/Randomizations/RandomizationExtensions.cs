125945f2f3043d1d1b35310851662ec4003f2d8c
Jean-Sylvain Boige <jsboige@gmail.com>
2021-05-06 13:44:50 +0200

    Added GetNormal extension to randomization

--- src/GeneticSharp.Domain/Randomizations/RandomizationExtensions.cs
+++ src/GeneticSharp.Domain/Randomizations/RandomizationExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace GeneticSharp.Domain.Randomizations
+﻿using System;
+
+namespace GeneticSharp.Domain.Randomizations
 {
     /// <summary>
     /// Extension methods for IRandomization.
@@ -36,6 +38,25 @@
                 var candidate = randomization.GetInt(min, max - 1);
 
                 return candidate % 2 != 0 ? candidate : candidate + 1;
+            }
+        }
+
+        /// <summary>
+        /// Gets a normally distributed random double from a uniform distribution using the Box-Muller algorithm.
+        /// </summary>
+        /// <returns>a random double</returns>
+        /// <param name="randomization">The IRandomization implementation.</param>
+        /// <param name="mean">The mean of the normal distribution</param>
+        /// <param name="std">The standard deviation of the normal distribution</param>
+        public static double GetNormal(this IRandomization randomization, double mean, double std)
+        {
+            checked
+            {
+                var u1 = randomization.GetDouble();
+                var u2 = randomization.GetDouble();
+                var z1 = Math.Sqrt((-2) * Math.Log(u1)) * Math.Cos(2 * Math.PI * u2);
+
+                return std*z1+mean;
             }
         }
     }
