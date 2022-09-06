d3b739e9e7db6731352387ce1d3ebd85ce43a72b
giacomelli <giacomelli@gmail.com>
2022-09-05 21:05:36 -0300

    Improving test stability

--- src/GeneticSharp.Domain.UnitTests/Randomizations/FastRandomRandomizationTest.cs
+++ src/GeneticSharp.Domain.UnitTests/Randomizations/FastRandomRandomizationTest.cs
@@ -72,16 +72,19 @@ namespace GeneticSharp.Domain.UnitTests.Randomizations
         [Repeat(10)]
         public void GetDouble_ManyThreads_DiffRandomResult()
         {
-            var target = new FastRandomRandomization();
-            var actual = new BlockingCollection<int>();
-
-            Parallel.For(0, 1000, (i) =>
+            FlowAssert.IsAtLeastOneAttemptOk(10, () =>
             {
-                actual.Add(target.GetInt(0, int.MaxValue));
-            });
+                var target = new FastRandomRandomization();
+                var actual = new BlockingCollection<int>();
 
-            Assert.AreEqual(1000, actual.Count);
-            Assert.AreEqual(1000, actual.Distinct().Count());
+                Parallel.For(0, 1000, (i) =>
+                {
+                    actual.Add(target.GetInt(0, int.MaxValue));
+                });
+
+                Assert.AreEqual(1000, actual.Count);
+                Assert.AreEqual(1000, actual.Distinct().Count());
+            });
         }
 
         [Test]
