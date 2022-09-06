9c9605e2df54aa65b0e95d4b0a25a90376a830a5
giacomelli <giacomelli@gmail.com>
2022-09-05 22:27:42 -0300

    Fixing nuget package folder

--- buildLibrariesNuget.cmd
+++ buildLibrariesNuget.cmd
@@ -1,5 +1,5 @@
 SET PACKAGE_VERSION="3.0.0"
 
 mkdir .\src\nuget
-dotnet pack src/GeneticSharp.Domain/GeneticSharp.Domain.csproj -c release --no-build --output ../nuget /p:PackageVersion=%PACKAGE_VERSION%
-dotnet pack src/GeneticSharp.Extensions/GeneticSharp.Extensions.csproj -c release --no-build --output ../nuget /p:PackageVersion=%PACKAGE_VERSION%
+dotnet pack src/GeneticSharp.Domain/GeneticSharp.Domain.csproj -c release --no-build --output ./src/nuget /p:PackageVersion=%PACKAGE_VERSION%
+dotnet pack src/GeneticSharp.Extensions/GeneticSharp.Extensions.csproj -c release --no-build --output ./src/nuget /p:PackageVersion=%PACKAGE_VERSION%
