381bc8e1fec614ec409f64d6ca0e5cfeb43a687d
giacomelli <giacomelli@gmail.com>
2022-09-09 06:14:57 -0300

    Fix #92 - Multiple occurances of same chromosome instance in generation

---
+++ src/GeneticSharp.Domain.UnitTests/Selections/SelectionStubChromosome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticSharp.Domain.UnitTests.Selections
+{
+    class SelectionStubChromosome : ChromosomeBase
+    {
+        public SelectionStubChromosome() : base(2)
+        {
+            Fitness = 0;
+        }
+
+        public override IChromosome CreateNew() => new SelectionStubChromosome();
+
+        public override Gene GenerateGene(int geneIndex) => new Gene(0);
+    }
+}
