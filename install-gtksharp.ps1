07b3783523a2139376b116e8ff3c132bd4fba9c0
giacomelli <giacomelli@gmail.com>
2023-01-29 05:30:06 -0300

    More log

--- install-gtksharp.ps1
+++ install-gtksharp.ps1
@@ -1,11 +1,12 @@
-Write-Output "Downloading installer..."
+Write-Output "INSTALLING GTK#"
+Write-Output "  Downloading installer..."
 Invoke-WebRequest -uri https://github.com/mono/gtk-sharp/releases/download/2.12.45/gtk-sharp-2.12.45.msi -OutFile ./gtk-sharp-2.12.45.msi 
 
-Write-Output "Starting installer..."
+Write-Output "  Starting installer..."
 ./gtk-sharp-2.12.45.msi /quiet /qn /norestart
 
-Write-Output "Waiting for installer finish..."
+Write-Output "  Waiting for installer finish..."
 Start-Sleep 10
 Wait-Process -Name "msiexec" -Timeout 300
 
-Write-Output "Done."
+Write-Output "GTK# INSTALLED"
