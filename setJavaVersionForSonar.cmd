c79037743688f02313f3fc0bd4887436cae4bab9
giacomelli <giacomelli@gmail.com>
2024-03-20 14:29:24 -0300

    Using Java 21 for Sonar

--- setJavaVersionForSonar.cmd
+++ setJavaVersionForSonar.cmd
@@ -1,5 +1,5 @@
-set JAVA_HOME=C:\Program Files\Java17
+set JAVA_HOME=C:\Program Files\Java21
 set Path=%JAVA_HOME%\bin;%Path%
-echo Java 17 activated.
+echo Java 21 activated.
 echo %JAVA_HOME%
 java -version
