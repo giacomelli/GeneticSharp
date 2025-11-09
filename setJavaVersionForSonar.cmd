c21f0818e794120ce1834126f2f62a64d1f9216a
giacomelli <giacomelli@gmail.com>
2025-11-09 06:45:35 -0300

    Fix some dependencies

--- setJavaVersionForSonar.cmd
+++
@@ -1,5 +0,0 @@
-set JAVA_HOME=C:\Program Files\Java\jdk21
-set Path=%JAVA_HOME%\bin;%Path%
-echo Java 21 activated.
-echo %JAVA_HOME%
-java -version
---
+++ setJavaVersionForSonar.ps1
@@ -0,0 +1,3 @@
+choco install temurin17 -y
+setx JAVA_HOME "C:\Program Files\Eclipse Adoptium\jdk-17.0.17.10-hotspot"
+setx PATH "%JAVA_HOME%\bin;%PATH%"
